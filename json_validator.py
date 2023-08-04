12100abe7278b7509f2a83aa97f527d46795bb9f
irannam <musigeriiranna@gmail.com>
2023-08-04 15:14:03 +0530

    fix:typing,reduced code, proper variable names added

--- json_validator.py
+++ json_validator.py
@@ -8,6 +8,8 @@ _SCHEMA_TYPE_MAP: Mapping[str, type] = MappingProxyType(
         "string": str,
         "integer": int,
         "boolean": bool,
+        "number":Union[int,float]
+       
     }
 )
 
@@ -15,40 +17,45 @@ json_schema: Json_Schema = {
     "type": "object",
     "properties": {
         "name": {"type": "string"},
-        "age": {"type": "integer"},
+        "age": {"type": "number"},
         "isMarried": {"type": "boolean"},
     },
 }
 
 json_data: Json_Schema = {
-    "name": "rtry",
-    "age": 90,
+    "name":"123",
+    "age": 45.0,
     "isMarried": True,
 }
 
 
 def json_schema_validator(json_schema: Json_Schema, json_data: Json_Schema) -> bool:
-    """main method for schema validator"""
-    try:
+      """main method for schema validator
+      Here we are validating
+      1.Object
+      2.String
+      3.Integer
+      4.Number
+      5.Boolean
+      """
+      try:
         if json_schema["type"] == "object":
-            for key in json_schema["properties"]:
+                properties=json_schema["properties"]
                 return all(
                     json_schema_validator(
                         json_schema["properties"][key], json_data[key]
                     )
-                    for key in json_data
+                    for key in properties
                 )
-        if json_schema["type"] == "number":
-            return isinstance(json_data, int) or isinstance(json_data, float)
 
         return isinstance(json_data, _SCHEMA_TYPE_MAP[json_schema["type"]])
-    except:
+      except KeyError:
         return True
 
 
 if json_schema_validator(json_schema, json_data):
-    val = "Valid"
+    is_valid = "Valid"
 else:
-    val = "Invalid"
+    is_valid = "Invalid"
 
 print("Given JSON data is " + val)
